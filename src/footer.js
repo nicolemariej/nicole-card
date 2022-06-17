2f7a4671c2bde066b33873ba461a7e35e68d2d07
Nicole <nicolemariejones1@gmail.com>
2022-06-17 13:31:00 +0200

    Add in icons

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,20 @@
 import React from "react"
+import { FaTwitterSquare} from 'react-icons/fa';
+import { FaFacebookSquare} from 'react-icons/fa';
+import { FaInstagramSquare} from 'react-icons/fa';
+import { FaGithubSquare} from 'react-icons/fa';
+
 
 
 export default function Footer(){
     return (
         <div>
-        
+        <FaTwitterSquare />
+        <FaFacebookSquare />
+        <FaInstagramSquare />
+        <FaGithubSquare />
+
+
             <h1>Footer</h1>
 
         </div>
--- src/info.js
+++ src/info.js
@@ -1,5 +1,7 @@
 import React from "react"
 import nicole from "./nicole.jpg"
+import { MdEmail} from 'react-icons/md';
+import { FaLinkedin} from 'react-icons/fa';
 
 export default function Info(){
     return (
@@ -9,8 +11,8 @@ export default function Info(){
             <h2>Frontend Developer</h2>
                 <a href="https://www.nicodes.io/" target="blank">Nicodes.io</a>
             <div className="contact-buttons">
-                <button href="mailto: nicodesjones@gmail.com" target="blank">Email</button>
-                <button href="https://www.linkedin.com/in/nicolemairejones/" target="blank">LinkedIn</button>
+                <button href="mailto: nicodesjones@gmail.com" target="blank"><MdEmail />Email</button>
+                <button href="https://www.linkedin.com/in/nicolemairejones/" target="blank"><FaLinkedin />LinkedIn</button>
             </div>
         
         </div>
