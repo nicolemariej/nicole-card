fc292dd8cfb3edcf689a051b3c7ddcc2b885c1dc
Nicole <nicolemariejones1@gmail.com>
2022-06-17 13:15:47 +0200

    html added to components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Info from './info';
 import About from './about';
 import Footer from './footer';
+import Interests from './interests';
 
 function App() {
   return (
     <div className="App">
       <Info />
       <About />
+      <Interests />
       <Footer />
     </div>
   );
--- src/info.js
+++ src/info.js
@@ -1,10 +1,18 @@
 import React from "react"
+import nicole from "./nicole.jpg"
 
 export default function Info(){
     return (
         <div>
-            <h1>Info</h1>
-
+            <img src={nicole} alt="Nicole" />
+            <h1>Nicole Jones</h1>
+            <h2>Frontend Developer</h2>
+                <a href="https://www.nicodes.io/" target="blank">Nicodes.io</a>
+            <div className="contact-buttons">
+                <button href="mailto: nicodesjones@gmail.com" target="blank">Email</button>
+                <button href="https://www.linkedin.com/in/nicolemairejones/" target="blank">LinkedIn</button>
+            </div>
+        
         </div>
     )
 }
